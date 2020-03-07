753935df0fbda506753bf590a0c0a3d9ce7fec4c
stefa <stefa@DESKTOP-J1FG61I>
2020-03-07 22:12:04 +0200

    Fix bug

--- api/app/core/App.php
+++ api/app/core/App.php
@@ -36,7 +36,7 @@ class App
         }
 
         unset($_GET['url']);
-        $this->crumbs = array_merge($_GET, $_POST);;
+        $this->crumbs = $this->getRequestParameters();
 
         $obj = new $this->path();
         $func = $this->direction;
@@ -48,5 +48,14 @@ class App
         if (isset($_GET['url'])) {
             return $url = explode('/', filter_var(rtrim($_GET['url'], '/'), FILTER_SANITIZE_URL));
         }
+    }
+
+    public function getRequestParameters()
+    {
+        $putDeleteParameters = file_get_contents("php://input");
+        $getParameters = $_GET;
+        $postParameters = $_POST;
+
+        return array_merge_recursive(json_decode($putDeleteParameters, true), $getParameters, $postParameters);
     }
 }
